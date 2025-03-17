09ff120ac673e0916313803bd73f5d69e40f1677
matsuokayuuki <matsu.yu-0114@ezweb.ne.jp>
2025-03-18 00:14:04 +0900

    node moduleを入れた

---
+++ src/bingo.tsx
@@ -0,0 +1,10 @@
+
+
+const Bingo: React.FC = () => {
+    const [numbers, setNumbers] = useState<number[]>(generateNumbers());
+    const [selectedNumbers, setSelectedNumbers] = useState<number[]>([]);
+
+    function
+
+}
+
